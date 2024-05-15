c04a347394ac727aa97dc6e901166ac9c0853af5
Felipe Fabricio <ff.oliveira32@gmail.com>
2024-05-15 00:32:23 -0300

    Configura Exclude from Coverage

--- src/Wonderfood.Worker/DependencyInjection.cs
+++ src/Wonderfood.Worker/DependencyInjection.cs
@@ -1,10 +1,8 @@
-using System.Diagnostics.CodeAnalysis;
 using System.Reflection;
 using Microsoft.OpenApi.Models;
 
 namespace Wonderfood.Worker;
 
-[ExcludeFromCodeCoverage]
 public static class DependencyInjection
 {
     public static void AddSwagger(this IServiceCollection services)
